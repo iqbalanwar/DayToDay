92cfe4e0ba098dddfdc4e5d344544e1dd1dcfa72
Iqbal <iqbalaanwar@gmail.com>
2019-11-05 10:32:39 -0500

    first commit

---
+++ dayToDay/src/main/java/com/example/dayToDay/DayToDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.dayToDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayToDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayToDayApplication.class, args);
+	}
+
+}
---
+++ dayToDay/src/test/java/com/example/dayToDay/DayToDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dayToDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayToDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
