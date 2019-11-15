15f6b8936d129396cd21e335f926c7806a988821
Iqbal <iqbalaanwar@gmail.com>
2019-11-14 19:44:04 -0500

    Microservices really works this time I swear

--- api-gateway/src/test/java/com/example/apigateway/ZuulGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/example/apigateway/ZuulGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.example.apigateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServerApplicationTests {
+public class ZuulGatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -2,16 +2,13 @@ package com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-@RestController
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
-
 }
